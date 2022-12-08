b5fedff7d5d69556fc0ffa6f755c95f39cf1a12d
sourabh <sourabh@gmail.com>
2022-12-08 18:07:28 +0000

    multiline comment change

--- 3_exe.sh
+++ 3_exe.sh
@@ -1,7 +1,7 @@
-""" 
+:" 
 Store the output of the command “hostname” in a variable. Display “This script is running on _.”
 where “_” is the output of the “hostname” command.
-"""
+"
 #! /bin/bash
 
 hostname=${whoami}
