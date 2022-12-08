9cde2413462bacafec9d230a9f0142f963a5ddb4
sourabh <sourabh@gmail.com>
2022-12-08 18:07:21 +0000

    multiline comment change

--- 2_exe.sh
+++ 2_exe.sh
@@ -1,7 +1,7 @@
-""" 
+: "
 Exercise_2 - Modify the shell script from exercise 1 to include a variable. 
 The variable will hold the contents of the message 'Shell Scripting is Fun!' 
-"""
+"
 
 #! /bin/bash
 
